3f81ff6f8a406523fa352d9d76d915bb54eab4e6
suvra2001 <112110876+suvra2001@users.noreply.github.com>
2023-01-26 19:08:14 -0500

    Demos

--- 19-week/19.1/01_Hello_World_Again/unsolved/js/index.js
+++ 19-week/19.1/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,5 @@
 // Create a variable below
-
+var hello = "Hello word!!! <3";
 // Console.log() your variable here.
+
+console.log(hello);
---
+++ 19-week/19.1/Demos/index.js
@@ -0,0 +1,4 @@
+var x = 20;
+var y = 10;
+var sum = x + y;
+console.log(sum);
