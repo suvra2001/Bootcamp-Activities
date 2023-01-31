e9af87f26e2c9c06bfdd0275d33eb6f73e996196
suvra2001 <112110876+suvra2001@users.noreply.github.com>
2023-01-30 19:09:04 -0500

    19.2.1 activity

--- 19-week/19.2/01_Conditionals/unsolved/js/index.js
+++ 19-week/19.2/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,16 @@
 $('.tab').on("click", function(){
-  
+  if ($(this).hasClass("tab1")) {
+    $(".tab1").addClass("active");
+    $(".tab2").removeClass("active");
+    $("#tabContent1").css("display", 
+    "block");
+    $("#tabContent2").css("display", "none");
+  }
+  else if ($(this).hasClass("tab2")) {
+    $(".tab1").removeClass("active");
+    $(".tab2").addClass("active");
+    $("#tabContent1").css("display", "none");
+    $("#tabContent2").css("display", "block");
+  }
+
 });
